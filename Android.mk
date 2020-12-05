e8affa68e7ca41777849c9f883d10d1cf4a5331d
g <gWOLF3@protonmail.com>
2020-12-04 19:51:50 -0600

    init

--- Android.mk
+++ Android.mk
@@ -1,9 +1,9 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := VPN
+LOCAL_MODULE := Forcastie
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_CLASS := APPS
-LOCAL_SRC_FILES := vpn.apk
+LOCAL_SRC_FILES := forcastie.apk
 include $(BUILD_PREBUILT)
